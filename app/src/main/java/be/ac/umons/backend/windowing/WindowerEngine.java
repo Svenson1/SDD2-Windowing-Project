9aa6f606407d6a155ad87e25995f268bb16c9603
Svenson1 <240273@umons.ac.be>
2026-03-28 19:32:15 +0100

    il reste l'engine à adapter

--- app/src/main/java/be/ac/umons/backend/windowing/WindowerEngine.java
+++ app/src/main/java/be/ac/umons/backend/windowing/WindowerEngine.java
@@ -4,6 +4,8 @@ import be.ac.umons.backend.geometry.HorizontalSegment;
 import be.ac.umons.backend.geometry.Point;
 import be.ac.umons.backend.geometry.Segment;
 import be.ac.umons.backend.pst.PST;
+import be.ac.umons.backend.pst.PstH;
+import be.ac.umons.backend.pst.PstV;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,27 +20,11 @@ public class WindowerEngine {
     private final PST pstH;
     private final PST pstV;
 
-    private final java.util.Map<Segment, Segment> invertedToOrigin;
-
     public WindowerEngine(List<Segment> horizontals, List<Segment> verticals) {
-        this.pstH = new PST();
-        this.pstV = new PST();
+        this.pstH = new PstH();
+        this.pstV = new PstV();
 
         pstH.build(horizontals);
-        pstV.build(invertSegments(verticals));
-    }
-
-    private List<Segment> invertSegments(List<Segment> segments) {
-        List<Segment> inverted = new ArrayList<>();
-        for (Segment s : segments) {
-            Point stored = s.getStoredPoint();
-            Point other = s.getOtherPoint();
-            Point invertedStored = new Point(stored.getY(), stored.getX());
-            Point invertedOther = new Point(other.getY(), other.getX());
-            Segment invertedSegment = new HorizontalSegment(invertedStored, invertedOther);
-            inverted.add(invertedSegment);
-            invertedToOrigin.put(invertedSegment, s);
-        }
-        return inverted;
+        pstV.build(verticals);
     }
 }
